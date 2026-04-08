250ec75e2cdceee9681fd313832ba08571040993
Alisha Soedamah <alisha.soedamah@gmail.com>
2026-04-08 16:28:01 +0200

    slay cyd

--- components/globe.js
+++ components/globe.js
@@ -9,7 +9,7 @@ const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
-document.body.appendChild(renderer.domElement);
+document.querySelector('main').appendChild(renderer.domElement);
 
 const ctrls = new OrbitControls(camera, renderer.domElement);
 ctrls.enableDamping = true;
@@ -23,16 +23,16 @@ scene.add(line);
 const material = new THREE.MeshStandardMaterial({
 
 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const globe = new THREE.Mesh(geometry, material);
+scene.add(globe);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
 scene.add(hemiLight);
 
 function animate() {
 	requestAnimationFrame(animate);
-	//   cube.rotation.x += 0.1;
-	//   cube.rotation.y += 0.2;
+	//   globe.rotation.x += 0.1;
+	//   globe.rotation.y += 0.2;
 	renderer.render(scene, camera);
 	ctrls.update();
 }
